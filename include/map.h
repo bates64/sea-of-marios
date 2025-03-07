ff71e531fe355c22502ab208427dd03b8476fce7
Alex Bates <alex@bates64.com>
2025-03-07 01:23:45 +0000

    fix gAreas length

--- include/map.h
+++ include/map.h
@@ -66,7 +66,7 @@ extern char wMapShapeName[];
 extern char wMapBgName[];
 
 /// Zero-terminated.
-extern AreaConfig gAreas[10];
+extern AreaConfig gAreas[9];
 
 extern EvtScript EVS_NpcHitRecoil;
 extern EvtScript EVS_800936C0;
